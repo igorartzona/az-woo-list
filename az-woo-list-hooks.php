73b4ada419564eaa38a854a176a9afa468d855e5
igor artzona <igor.artzona@gmail.com>
2019-11-08 14:14:12 +0300

    0.1.0

---
+++ az-woo-list-hooks.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('ABSPATH'))  exit;
+
+//add_action('woocommerce_before_shop_loop_item', 'az_awlv_image' , 5 );
+//add_action('woocommerce_before_shop_loop_item_title', 'az_awlv_end_div', 15 );
+
+
